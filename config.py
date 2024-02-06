57739a6580b5825bbc21f9db4d90283d8e37cad2
TaPXoBu4 <maldini-83@mail.ru>
2024-02-06 10:44:39 +0300

    удалил конфиг из индекса

--- config.py
+++
@@ -1,9 +0,0 @@
-from states import OperatorSG, WorkerSG, CustomerSG
-
-TOKEN = '6525353343:AAHW8JVm3wya_x52NdUXM5lAuBqZX-afgL8'
-
-START_STATES = {
-    'operator': OperatorSG.main,
-    'worker': WorkerSG.main,
-    'customer': CustomerSG.main
-}
