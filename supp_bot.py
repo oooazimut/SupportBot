e6a28ba1c15334829fd3c919fb273b458892cdc8
Марат <maldini-83@mail.ru>
2024-03-14 09:37:27 +0300

    подправил редис-хранилище

--- supp_bot.py
+++ supp_bot.py
@@ -19,7 +19,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 
 async def main():
     bot = Bot(config.TOKEN)
-    storage = RedisStorage(Redis(), key_builder=DefaultKeyBuilder(with_destiny=True))
+    storage = RedisStorage(Redis(), key_builder=DefaultKeyBuilder(with_destiny=True, with_bot_id=True))
     dp = Dispatcher(storage=storage)
     dp.include_routers(start_router.router)
     dp.include_routers(customers.main_dialog, customers.create_task_dialog, customers.task_dialog)
