765f628f372587b8988f0f3da2bc97d98addb48a
Марат <maldini-83@mail.ru>
2024-06-24 09:05:19 +0300

    config

--- config.py
+++
@@ -1,9 +0,0 @@
-from states import OperatorSG, WorkerSG, CustomerSG
-
-TOKEN = '6116372134:AAFvsyCXrrkvrOy_LrWqgJQm3F8gqJwyjZI'
-
-START_STATES = {
-    'operator': OperatorSG.main,
-    'worker': WorkerSG.main,
-    'customer': CustomerSG.main
-}
